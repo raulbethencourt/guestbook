4405595716c0eb267fc983e5bc48be104866dcb7
raulbethencourt <raul.bethencourt.pro@gmail.com>
2024-08-15 20:13:43 +0200

    feat(form): add form with photo handling

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,16 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Conference;
+use App\Form\CommentType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ConferenceController extends AbstractController
 {
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+    ) {}
+
     /**
      * This is the index action method of a controller.
      * It renders the 'conference/index.html.twig' template and passes the list of all conferences to it.
@@ -26,22 +34,33 @@ class ConferenceController extends AbstractController
         return $this->render('conference/index.html.twig');
     }
 
-    /**
-     * This controller action is responsible for rendering the conference details page.
-     *
-     * @param Request $request The current HTTP request
-     * @param Conference $conference The conference entity to be displayed
-     * @param CommentRepository $commentRepository The repository for fetching comments
-     *
-     * @return Response The HTTP response containing the rendered template
-     */
     #[Route('/conference/{slug}', name: 'conference')]
     public function show(
         Request $request,
         Conference $conference,
         CommentRepository $commentRepository,
-        ConferenceRepository $conferenceRepository
+        ConferenceRepository $conferenceRepository,
+
+        #[Autowire('%photo_dir%')]
+        string $photoDir,
     ): Response {
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setConference($conference);
+            if ($photo = $form['photo']->getData()) {
+                $fileName = bin2hex(random_bytes(6)) . '.' . $photo->guessExtension();
+                $photo->move($photoDir, $fileName);
+                $comment->setPhotoFilename($fileName);
+            }
+
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+        }
+
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
 
@@ -50,6 +69,7 @@ class ConferenceController extends AbstractController
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::COMMENTS_PER_PAGE,
             'next' => min(count($paginator), $offset + CommentRepository::COMMENTS_PER_PAGE),
+            'comment_form' => $form,
         ]);
     }
 }
